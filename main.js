8d579b5e23e7b18873ff59e8509a7d097738bd72
benjaminet <benjamintimmermann@gmail.com>
2018-11-12 13:15:27 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 /*
 To do:
 
-Finish the 3rd assignment --> 
-1. make sure the description is right compared to what we have coded so far and what we expect
-2. make sure the UML is correct 
 
 Programming: 
 
@@ -18,7 +15,7 @@ Programming:
 4 fix the register page
 . 
 
-
+IMPLEMENT DATES TO SELECT ON SINGLE RENTAL 
 
 Er UML korrekt?
 Skal man kunne se sin booking ift UML? 
@@ -31,7 +28,7 @@ Hvordan linker man videre til en valgt rental?
 
 
 // Rental class (with attributes)
-function Rental(country, city, type, title, price, guests, imageUrl, rooms, smokingIsAllowed, smokingIsNotAllowed, hasWifi){ //put all attributes from UML classdiagram in here
+function Rental(country, city, type, title, price, guests, imageUrl, rooms, smoking, hasWifi, hasLaundry, hasTV, hasAircon){ //put all attributes from UML classdiagram in here
     this.country = country;
     this.city = city;
     this.type = type;
@@ -40,9 +37,12 @@ function Rental(country, city, type, title, price, guests, imageUrl, rooms, smok
     this.guests = guests;
     this.imageUrl = imageUrl;
     this.rooms = rooms;
-    this.smokingIsAllowed = "Yes";
-    this.smokingIsNotAllowed = "No";
-    this.hasWifi = "Yes";
+    this.smoking = smoking;
+    this.hasWifi = hasWifi;
+    this.hasLaundry = hasLaundry;
+    this.hasTV = hasTV;
+    this.hasAircon = hasAircon;
+    //this.date = date; ???
 }
 
 // object containing all possible countries
@@ -63,24 +63,24 @@ var rentalTypes = {
 
 // Harcoded data (rentals):
 // france
-var france1 = new Rental(countries.France, "Carlux", rentalTypes.Castle, "Chateau De Ruffiac", 715, 12, "https://www.oliverstravels.com/uploads/herder_image/crop_9999_1000_Chateau-de-Ruffiac-Dordogne-Fr-Olivers-Travels-1.jpg");
-var france2 = new Rental(countries.France, "Carlux", rentalTypes.Castle, "Domaine Du Pont", 5500, 20, "https://www.oliverstravels.com/uploads/herder_image/crop_720_440_La-Maison-des-Fleurs-Atlantic-Coast-Olivers-Travels-1.jpg");
-var france3 = new Rental(countries.France, "Atlantic Coast", rentalTypes.Villa, "La Maison Des Fleurs ", 1300, 8, "https://www.oliverstravels.com/uploads/herder_image/crop_720_440_Chateau-Du-Chevalier-Only-Brittany-Olivers-Travels__1_.jpg");
+var france1 = new Rental(countries.France, "Carlux", rentalTypes.Castle, "Chateau De Ruffiac", 715, 12, "https://www.oliverstravels.com/uploads/herder_image/crop_9999_1000_Chateau-de-Ruffiac-Dordogne-Fr-Olivers-Travels-1.jpg", 6, "Allowed", "Yes", "No", "Yes", "No");
+var france2 = new Rental(countries.France, "Carlux", rentalTypes.Castle, "Domaine Du Pont", 5500, 20, "https://www.oliverstravels.com/uploads/herder_image/crop_720_440_La-Maison-des-Fleurs-Atlantic-Coast-Olivers-Travels-1.jpg", 11, "Not allowed", "Yes", "Yes", "No", "Yes");
+var france3 = new Rental(countries.France, "Atlantic Coast", rentalTypes.Villa, "La Maison Des Fleurs ", 1300, 8, "https://www.oliverstravels.com/uploads/herder_image/crop_720_440_Chateau-Du-Chevalier-Only-Brittany-Olivers-Travels__1_.jpg", 3, "Allowed", "Yes", "No", "Yes", "Yes");
 
 // Australia
-var australia1 = new Rental(countries.Australia, "Sydney", rentalTypes.Apartment, "Absolute Waterfront", 650, 6, "https://pictures.luxuryretreats.com/122208/Sydney_AbsoluteWaterfront-1.jpg");
-var australia2 = new Rental(countries.Australia, "Sydney", rentalTypes.Villa, "Vaucluse Luxury", 750, 10, "https://pictures.luxuryretreats.com/121296/Sydney_VaucluseHopetoun_01.jpg");
-var australia3 = new Rental(countries.Australia, "Melbourne", rentalTypes.Villa, "Bronte Sands", 425, 6, "https://pictures.luxuryretreats.com/121850/Sydney_BronteSands-1.jpg");
+var australia1 = new Rental(countries.Australia, "Sydney", rentalTypes.Apartment, "Absolute Waterfront", 650, 6, "https://pictures.luxuryretreats.com/122208/Sydney_AbsoluteWaterfront-1.jpg", 3, "Allowed", "Yes", "Yes", "Yes", "Yes");
+var australia2 = new Rental(countries.Australia, "Sydney", rentalTypes.Villa, "Vaucluse Luxury", 750, 10, "https://pictures.luxuryretreats.com/121296/Sydney_VaucluseHopetoun_01.jpg", 4, "Not allowed", "Yes", "No", "Yes", "Yes");
+var australia3 = new Rental(countries.Australia, "Melbourne", rentalTypes.Villa, "Bronte Sands", 425, 6, "https://pictures.luxuryretreats.com/121850/Sydney_BronteSands-1.jpg", 3, "Not allowed", "Yes", "No", "Yes", "Yes");
 
 // Dubai
-var dubai1 = new Rental(countries.Dubai, "Downtown", rentalTypes.Apartment, "Unique Luxury Apartment Downtown", 860, 7, "https://pictures.luxuryretreats.com/120330/Dubai_UniqueLuxuryApartmentDowntown_01.jpg");
-var dubai2 = new Rental(countries.Dubai, "Downtown", rentalTypes.Villa, "Jumeirah Villa", 1000, 10, "https://pictures.luxuryretreats.com/119472/Dubai_Private4BedVilla_01.jpg");
-var dubai3 = new Rental(countries.Dubai, "The Palm Jumeirah", rentalTypes.Villa, "Signature Villa", 920, 11, "https://pictures.luxuryretreats.com/121444/Dubai_SignatureVilla_50.jpg");
+var dubai1 = new Rental(countries.Dubai, "Downtown", rentalTypes.Apartment, "Unique Luxury Apartment Downtown", 860, 7, "https://pictures.luxuryretreats.com/120330/Dubai_UniqueLuxuryApartmentDowntown_01.jpg", 3, "Not allowed", "Yes", "Yes", "Yes", "Yes");
+var dubai2 = new Rental(countries.Dubai, "Downtown", rentalTypes.Villa, "Jumeirah Villa", 1000, 10, "https://pictures.luxuryretreats.com/119472/Dubai_Private4BedVilla_01.jpg", 4, "Allowed", "Yes", "Yes", "No", "Yes");
+var dubai3 = new Rental(countries.Dubai, "The Palm Jumeirah", rentalTypes.Villa, "Signature Villa", 920, 11, "https://pictures.luxuryretreats.com/121444/Dubai_SignatureVilla_50.jpg", 6, "Allowed", "Yes", "Yes", "Yes", "Yes");
 
 // Netherlands
-var netherlands1 = new Rental(countries.Netherlands, "Amsterdamn", rentalTypes.Houseboat, "Experience a houseboat in Amsterdam", 370, 5, "https://a0.muscache.com/im/pictures/95957379/b725845f_original.jpg?aki_policy=xx_large");
-var netherlands2 = new Rental(countries.Netherlands, "Amsterdam", rentalTypes.Apartment, "Lovely hideout in the middle of AMS", 450, 6, "https://a0.muscache.com/im/pictures/8cbb08e8-257c-45c9-895f-0607a126fe19.jpg?aki_policy=xx_large");
-var netherlands3 = new Rental(countries.Netherlands, "Lijnden", rentalTypes.Villa, "Private Luxurious house gardenview", 550, 8, "https://a0.muscache.com/im/pictures/3d6b10f7-f07b-4d07-88a2-ec748b26c894.jpg?aki_policy=xx_large");
+var netherlands1 = new Rental(countries.Netherlands, "Amsterdamn", rentalTypes.Houseboat, "Experience a houseboat in Amsterdam", 370, 5, "https://a0.muscache.com/im/pictures/95957379/b725845f_original.jpg?aki_policy=xx_large", 2, "Allowed", "Yes", "No", "No", "No");
+var netherlands2 = new Rental(countries.Netherlands, "Amsterdam", rentalTypes.Apartment, "Lovely hideout in the middle of AMS", 450, 6, "https://a0.muscache.com/im/pictures/8cbb08e8-257c-45c9-895f-0607a126fe19.jpg?aki_policy=xx_large", 3, "Allowed", "Yes", "No", "Yes", "Yes");
+var netherlands3 = new Rental(countries.Netherlands, "Lijnden", rentalTypes.Villa, "Private Luxurious house gardenview", 550, 8, "https://a0.muscache.com/im/pictures/3d6b10f7-f07b-4d07-88a2-ec748b26c894.jpg?aki_policy=xx_large", 4, "Allowed", "Yes", "Yes", "Yes", "Yes");
 
 // allRentals is a list of all rentals in the system (created above)
 var allRentals = [];
@@ -120,7 +120,7 @@ function generateAndShowRentalHtmlOverview(rental) {
 // function for generating detailed html from hardcoded rentals
 // using string literals https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 function generateAndShowRentalHtmlDetailed(rental) {
-    $("#content").append(`                          <!--- #content = ID selector in jquery https://api.jquery.com/id-selector/ --->
+    $("#content").append(`                                    <!--- #content = ID selector in jquery https://api.jquery.com/id-selector/ --->
     <div class="col-lg-12">
         <button type="button" class="btn btn-info" onclick="filterRentals()">Back</button>
     </div> 
@@ -132,8 +132,13 @@ function generateAndShowRentalHtmlDetailed(rental) {
         <p class='title'>${rental.title}</p>
         <p class='price'>${rental.price} GBP per night</p>
         <p class='guests'>${rental.guests} guests</p>
-        <p class='guests'>Smoking? ${rental.smokingIsAllowed}</p>
-        <p class='guests'>Wifi? ${rental.hasWifi}</p>
+        <p class='smoking'>Smoking? ${rental.smoking}</p>
+        <p class='wifi'>Wifi? ${rental.hasWifi}</p>
+        <p class='laundry'>Laundry? ${rental.hasLaundry}</p>
+        <p class='tv'>TV? ${rental.hasTV}</p>
+        <p class='aircon'>Aircondition? ${rental.hasAircon}</p>
+        
+        
     </div>
     `)
 }
